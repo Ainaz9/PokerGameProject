29bf8fec55a1981effbac62b10f619adac97ea9e
Никита Боронин <nikitab157@outlook.com>
2025-06-04 21:57:10 +0300

    сохранение перед конфликтами

---
+++ Models/CardSuitNOTUSED.cs
@@ -0,0 +1,13 @@
+﻿namespace PokerGameRSF.Models
+{
+    /// <summary>
+    /// Масти карт
+    /// </summary>
+    public enum CardSuitNOTUSED
+    {
+        Hearts, // Черви
+        Diamonds, // Бубны
+        Clubs, // Трефы
+        Spades // Пики
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Username { get; set; }
         public int Rating { get; set; } = 0;
-        public decimal Chips { get; set; } = 1000; // Starting chips
+        public decimal Chips { get; set; } = 10000; // Starting chips
         public virtual ICollection<PlayerCard> PlayerCards { get; set; }
         public virtual ICollection<PlayerAction> PlayerActions { get; set; }
         public virtual ICollection<Bet> Bets { get; set; }
